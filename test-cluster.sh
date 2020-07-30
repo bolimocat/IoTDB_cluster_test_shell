d3d1b67b00d8c35cc00532be9fdf908d481b86b3
mingli_a@163.com <mingli_a@163.com>
2020-07-30 16:17:58 +0800

    loop start

--- test-cluster.sh
+++ test-cluster.sh
@@ -17,10 +17,13 @@ timestamp=`cat $home/timestamp`
 			then echo 'Test cluster ddl'
 			while read sqlStr
 			do
+		# start cluster
+			source ./start-cluster.sh
 			echo ' --------------    Test DDL  [   '$sqlStr'   ]  Start  ----------------------'>>$home/result/ddl_${arrayTestType[1]}_out
 				ssh $user@${array_node[$[$RANDOM%5]]} '/home/'$user'/'$cliPath'/sbin/start-cli.sh -u root -pw root -h  '${array_node[$[$RANDOM%5]]} '  -p 55560 -e "'$sqlStr'"  ' >>$home/result/ddl_${arrayTestType[1]}_out                 
 			echo ' --------------    Test DDL  [   '$sqlStr'   ]   End   ----------------------'>>$home/result/ddl_${arrayTestType[1]}_out
-	
+		# stop cluster
+			source ./stop-cluster.sh
 			done<$home/$testCase/ddl_test/${arrayTestType[1]}
 			echo ' '>>$home/result/ddl_${arrayTestType[1]}_out
 			echo ' '>>$home/result/ddl_${arrayTestType[1]}_out
@@ -31,12 +34,18 @@ timestamp=`cat $home/timestamp`
 			then echo 'Test cluster dml'
 			while read sqlStr
 			do
+		# start cluster
+			source ./start-cluster.sh
+
 			echo ' --------------    Test DML   [  '$sqlStr'   ]   Start ----------------------'>>$home/result/dml_${arrayTestType[1]}_out
 				for((i=0;i<clusterLength;i++));
 				do
 					ssh $user@${array_node[i]} '/home/'$user'/'$cliPath'/sbin/start-cli.sh -u root -pw root -h  '${array_node[i]} '  -p 55560 -e "'$sqlStr'"  ' >>$home/result/dml_${arrayTestType[1]}_out 
 				done
 			echo ' --------------    Test DDL    [   '$sqlStr'    ] End ----------------------'>>$home/result/dml_${arrayTestType[1]}_out
+		# stop cluster
+			source ./stop-cluster.sh
+
 			done<$home/$testCase/ddl_test/${arrayTestType[1]}
 			echo ' '>>$home/result/dml_${arrayTestType[1]}_out
 			echo ' '>>$home/result/dml_${arrayTestType[1]}_out
